acb52a4889c5bd068939e5cfdb4fbd947c222434
João Debussy <social.debussy@gmail.com>
2023-02-12 19:50:20 -0300

    feat: create store function on controller

--- api/src/routes.js
+++ api/src/routes.js
@@ -6,6 +6,7 @@ const router = Router();
 
 router.get('/', ContactController.index);
 router.get('/contacts/:id', ContactController.show);
+router.post('/contacts', ContactController.store);
 router.delete('/contacts/:id', ContactController.delete);
 
 module.exports = router;
