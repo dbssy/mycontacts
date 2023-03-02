a68b6a517ec0d68d77de5e47b2e98bf1f5cad709
João Debussy <social.debussy@gmail.com>
2023-03-02 20:50:26 -0300

    feat: working with controlled components

--- web/src/components/ContactForm/index.jsx
+++ web/src/components/ContactForm/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { ButtonContainer, Form } from './styles';
@@ -8,20 +9,32 @@ import Select from '../Select';
 import Button from '../Button';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <FormGroup>
         <Input
           type="text"
           placeholder="Nome"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
         />
       </FormGroup>
 
-      <FormGroup error="O formato do e-mail é inválido.">
+      <FormGroup>
         <Input
           type="email"
           placeholder="E-mail"
-          error
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
         />
       </FormGroup>
 
@@ -29,12 +42,20 @@ export default function ContactForm({ buttonLabel }) {
         <Input
           type="text"
           placeholder="Telefone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
         />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
           <option value="">Categoria</option>
+          <option value="facebook">Facebook</option>
+          <option value="instagram">Instagram</option>
+          <option value="linkedin">LinkedIn</option>
         </Select>
       </FormGroup>
 
