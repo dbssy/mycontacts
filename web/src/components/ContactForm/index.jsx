1a71449a0db8d17661a7fa40006101ce0df406f7
João Debussy <social.debussy@gmail.com>
2023-03-07 19:09:56 -0300

    feat: implementing contact creation

--- web/src/components/ContactForm/index.jsx
+++ web/src/components/ContactForm/index.jsx
@@ -13,7 +13,7 @@ import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
 
-export default function ContactForm({ buttonLabel }) {
+export default function ContactForm({ buttonLabel, onSubmit }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
@@ -69,6 +69,8 @@ export default function ContactForm({ buttonLabel }) {
 
   function handleSubmit(event) {
     event.preventDefault();
+
+    onSubmit({ name, email, phone, categoryId });
   }
 
   return (
@@ -136,4 +138,5 @@ export default function ContactForm({ buttonLabel }) {
 
 ContactForm.propTypes = {
   buttonLabel: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
 };
