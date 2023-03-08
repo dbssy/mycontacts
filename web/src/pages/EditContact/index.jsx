cabb173a21bb33a6703d9584497b825b0f0ffb79
João Debussy <social.debussy@gmail.com>
2023-03-08 20:18:33 -0300

    feat: Loading contacts within the editing page

--- web/src/pages/EditContact/index.jsx
+++ web/src/pages/EditContact/index.jsx
@@ -1,15 +1,53 @@
+import { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
+
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import Loader from '../../components/Loader';
 
 export default function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(id);
+        console.log(contactData);
+
+        setIsLoading(false);
+      } catch {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado!',
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  function handleSubmit() {
+    //
+  }
+
   return (
     <>
+      <Loader isLoading={isLoading} />
+
       <PageHeader
         title="Editar João"
       />
 
       <ContactForm
         buttonLabel="Salvar alterações"
+        onSubmit={handleSubmit}
       />
     </>
   );
