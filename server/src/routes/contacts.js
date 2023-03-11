db2bad2bb35820b664f9bc0353e559cd7431d7c3
João Debussy <social.debussy@gmail.com>
2023-03-11 11:51:23 -0300

    refactor: edited the contacts index route

--- server/src/routes/contacts.js
+++ server/src/routes/contacts.js
@@ -4,11 +4,11 @@ const router = Router();
 
 const ContactController = require('../app/controllers/ContactController');
 
-router.get('/contacts', ContactController.index);
-router.get('/contacts/:id', ContactController.show);
+router.get('/', ContactController.index);
+router.get('/:id', ContactController.show);
 
-router.post('/contacts', ContactController.store);
-router.put('/contacts/:id', ContactController.update);
-router.delete('/contacts/:id', ContactController.delete);
+router.post('/', ContactController.store);
+router.put('/:id', ContactController.update);
+router.delete('/:id', ContactController.delete);
 
 module.exports = router;
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -5,7 +5,7 @@ const router = Router();
 const contacts = require('./contacts');
 const categories = require('./categories');
 
-router.use('/', contacts);
+router.use('/contacts', contacts);
 router.use('/categories', categories);
 
 module.exports = router;
