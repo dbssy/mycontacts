e366a10d14a9fe3906b32865372c734189bc2d25
João Debussy <social.debussy@gmail.com>
2023-03-21 19:23:06 -0300

    refactor: canceling requests with AbortController

--- web/src/pages/EditContact/useEditContact.js
+++ web/src/pages/EditContact/useEditContact.js
@@ -16,9 +16,14 @@ export default function useEditContact() {
   const safeAsyncAction = useSafeAsyncAction();
 
   useEffect(() => {
+    const controller = new AbortController();
+
     async function loadContact() {
       try {
-        const contact = await ContactsService.getContactById(id);
+        const contact = await ContactsService.getContactById(
+          id,
+          controller.signal,
+        );
 
         safeAsyncAction(() => {
           contactFormRef.current.setFieldsValues(contact);
@@ -26,7 +31,11 @@ export default function useEditContact() {
           setIsLoading(false);
           setContactName(contact.name);
         });
-      } catch {
+      } catch (error) {
+        if (error instanceof DOMException && error.name === 'AbortError') {
+          return;
+        }
+
         safeAsyncAction(() => {
           history.push('/');
           toast({
@@ -38,6 +47,10 @@ export default function useEditContact() {
     }
 
     loadContact();
+
+    return () => {
+      controller.abort();
+    };
   }, [id, history, safeAsyncAction]);
 
   async function handleSubmit(contact) {
